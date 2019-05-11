736b80b23ddf388b542d8b285fbf52583751c804
Bigyan <bigyanshr@gmail.com>
2019-05-11 09:32:53 +0545

    Added method to create superuser

--- app/core/models.py
+++ app/core/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import (
-    AbstractBaseUser, BaseUserManager,
-    PermissionsMixin
+    AbstractBaseUser, BaseUserManager, PermissionsMixin
 )
 
 
@@ -15,6 +14,15 @@ class UserManager(BaseUserManager):
 
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """ Creates and saves a new superuser. """
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
